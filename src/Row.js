d4299d4be8a6c4429a3d5d97fa5c943863965c48
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com1 git config --global user.email oyazicioglu@hotmail.com git config --global user.email oyazicioglu@hotmail.com1>
2022-04-22 20:19:04 +0300

    Add setters in Row class

--- src/Row.js
+++ src/Row.js
@@ -5,11 +5,53 @@ export class Row {
     index;
 
     /** @type {Column[]} */
-    coumns;
+    columns;
 
     /** @type {boolean} */
     selectable;
 
     /** @type {boolean} */
     visible;
+
+    constructor({ index, columns = [], selectable = true, visible = true }) {
+        this.index = index;
+        this.columns = columns;
+        this.selectable = selectable;
+        this.visible = visible;
+    }
+
+    /**
+     * @param {number} index
+     */
+    setIndex(index) {
+        this.index = index;
+    }
+
+    /**
+     * @param {Column[]} columns
+     */
+    setColumns(columns) {
+        this.columns = columns;
+    }
+
+    /**
+     * @param {Column} column
+     */
+    addColumn(column) {
+        this.columns.push(column);
+    }
+
+    /**
+     * @param {boolean} selectable
+     */
+    setSelectable(selectable) {
+        this.selectable = selectable;
+    }
+
+    /**
+     * @param {boolean} visible
+     */
+    setVisible(visible) {
+        this.visible = visible;
+    }
 }
